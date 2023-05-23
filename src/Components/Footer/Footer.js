a9da2439df46d463d71084436f3cd1c82d45b016
elj-Wissal <wissaleljirari0@gmail.com>
2023-05-23 15:19:57 +0100

    done

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./Footer.css"
+import { Link } from "react-router-dom"
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <ul className='footer-list'>
+                <li><h2>ABOUT</h2></li>
+                <li><p>Contact us</p></li>
+                <li><p>About us</p></li>
+                <li><p>Careers</p></li>
+                <li><p>Gift Cards</p></li>
+            </ul>
+            <ul className='footer-list'>
+                <li><h2>HELP</h2></li>
+                <li><p>Payments</p></li>
+                <li><p>Shipping</p></li>
+                <li><p>Cancellation & Returns</p></li>
+                <li><p>FAQs</p></li>
+            </ul>
+            <ul className='footer-list'>
+                <li><h2>SOCIALS</h2></li>
+                <li>
+                        <p>Linkedin</p>
+                
+                </li>
+                <li>
+                    <a href="https://github.com/elj-Wissal" rel="noreferrer" target="_blank">
+                        <p>Github</p>
+                    </a>
+                </li>
+                <li>
+                        <p>Twitter</p>
+                    
+                </li>
+                <li>
+                        <p>Instagram</p>
+                    
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export { Footer } 
